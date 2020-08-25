422c577c2670b75616af64e183c5efd7b27b960b
victor <894609987@qq.com>
2020-08-25 13:07:29 +0800

    new

--- Practice.java
+++ Practice.java
@@ -1,12 +1,11 @@
 
-
-
 import java.util.Scanner;
 public class Practice{
 	public static void main(String[] args){
 		Scanner  input = new Scanner(System.in);
 		int i = input.nextInt();
 		System.out.println(i);
+		
 	}
 }
 
