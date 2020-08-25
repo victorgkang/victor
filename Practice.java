32661d93a6ac89fb26a26d6657a95a8289779482
victor <894609987@qq.com>
2020-08-25 12:46:56 +0800

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,12 @@
+
+
+
+import java.util.Scanner;
+public class Practice{
+	public static void main(String[] args){
+		Scanner  input = new Scanner(System.in);
+		int i = input.nextInt();
+		System.out.println(i);
+	}
+}
+
